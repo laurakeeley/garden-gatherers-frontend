208247c3f535fc413bae7a1d3036ffdae448134e
laura <l.keeley000@gmail.com>
2021-10-29 10:47:12 -0500

    adds login

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Garden Gatherers</router-link> |
       <router-link to="/about">About</router-link> |
+      <router-link to="/login">Login</router-link> |
       <router-link to="/signup">Signup</router-link>
     </div>
     <router-view />
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -21,6 +22,7 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   { path: "/signup", name: "signup", component: Signup },
+  { path: "/login", name: "login", component: Login },
 ];
 
 const router = new VueRouter({
