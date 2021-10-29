e6d0b70994b91a4ec4ec9e523d8076adad44f2b2
laura <l.keeley000@gmail.com>
2021-10-29 12:11:14 -0500

    adds comments show to post

--- src/views/PostsShow.vue
+++ src/views/PostsShow.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="posts-show">
-    <h2>{{ post.title }}</h2>
-    <p>{{ post.body }}</p>
-    <img v-bind:src="post.image_url" v-bind:alt="post.title" />
-    <p>{{ post.created_at }}</p>
-    <p>{{ post.updated_at }}</p>
+    <div>
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+      <img v-bind:src="post.image_url" v-bind:alt="post.title" />
+      <p>{{ post.created_at }}</p>
+      <p>{{ post.updated_at }}</p>
+    </div>
+    <h3>Comments</h3>
+    <div v-for="comment in post.comments" v-bind:key="comment.id">
+      <p>{{ comment.body }}</p>
+      <p>{{ comment.image_url }}</p>
+      <p>{{ comment.created_at }}</p>
+      <p>{{ comment.updated_at }}</p>
+      <p>{{ comment.user.name }}</p>
+      <p>{{ comment.user.location }}</p>
+      <img v-bind:src="comment.user.image_url" v-bind:alt="comment.user" />
+    </div>
   </div>
 </template>
 
