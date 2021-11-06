f4fdfe45fd1de7aa11f563a9186ba4579b9e73eb
laura <l.keeley000@gmail.com>
2021-11-06 13:56:16 -0500

    adds catch to login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -30,13 +30,19 @@ export default {
   },
   methods: {
     submit: function () {
-      axios.post("/sessions", this.newSessionParams).then((response) => {
-        axios.defaults.headers.common["Authorization"] =
-          "Bearer " + response.data.jwt;
-        localStorage.setItem("jwt", response.data.jwt);
-        localStorage.setItem("user_id", response.data.user_id);
-        this.$router.push("/");
-      });
+      axios
+        .post("/sessions", this.newSessionParams)
+        .then((response) => {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          localStorage.setItem("user_id", response.data.user_id);
+          this.$router.push("/");
+        })
+        .catch((error) => {
+          console.log("login error", error.response);
+          this.errors = error.response.data.errors;
+        });
     },
   },
 };
